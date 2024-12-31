f0e151e07a573e28b5f0476b9325a56967dbede8
ghiyas1337 <ghiyasalok@gmail.com>
2024-12-31 10:58:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -22,20 +22,22 @@ def clear_screen():
 def print_banner():
     banner = """[bold red]
 
-▒▒▒▒▒▒▒▒▄▄▄▄▄▄▄▄▒▒▒▒▒▒  [bright_yellow]╔══════════════════════════════╗[/]
-▒▒█▒▒▒▄██████████▄▒▒▒▒  [bright_yellow]║[/]    [bold cyan]DoS Testing Framework[/]     [bright_yellow]║[/]
-▒█▐▒▒▒████████████▒▒▒▒  [bright_yellow]║[/]   [bold red]Created by Ghiyas1337[/]     [bright_yellow]║[/]
-▒▌▐▒▒██▄▀██████▀▄██▒▒▒  [bright_yellow]╚══════════════════════════════╝[/]
-▐┼▐▒▒██▄▄▄▄██▄▄▄▄██▒▒▒  [bright_yellow][[/][bold red]WARNING[/][bright_yellow]][/] [bold white]Educational Purposes Only[/]
-▐┼▐▒▒██████████████▒▒▒  [bright_yellow][[/][bold cyan]INFO[/][bright_yellow]][/] [bold white]Press Ctrl+C to Stop Attack[/]
-▐▄▐████─▀▐▐▀█─█─▌▐██▄▒  [bright_yellow][[/][bold green]MODE[/][bright_yellow]][/] [bold white]Unlimited Threads | Auto-Timer[/]
-▒▒█████──────────▐███▌
-▒▒█▀▀██▄█─▄───▐─▄███▀▒
-▒▒█▒▒███████▄██████▒▒▒
-▒▒▒▒▒██████████████▒▒▒
-▒▒▒▒▒█████████▐▌██▌▒▒▒
-▒▒▒▒▒▐▀▐▒▌▀█▀▒▐▒█▒▒▒▒▒
-▒▒▒▒▒▒▒▒▒▒▒▐▒▒▒▒▌▒▒▒▒▒
+   █████╗ ██╗  ██╗██╗██╗   ██╗ █████╗ ███████╗ ██╗██████╗ ██████╗ ███████╗
+██╔════╝ ██║  ██║██║╚██╗ ██╔╝██╔══██╗██╔════╝███║╚════██╗╚════██╗╚════██║
+██║  ███╗███████║██║ ╚████╔╝ ███████║███████╗╚██║ █████╔╝ █████╔╝    ██╔╝
+██║   ██║██╔══██║██║  ╚██╔╝  ██╔══██║╚════██║ ██║ ╚═══██╗ ╚═══██╗   ██╔╝ 
+╚██████╔╝██║  ██║██║   ██║   ██║  ██║███████║ ██║██████╔╝██████╔╝   ██║  
+ ╚═════╝ ╚═╝  ╚═╝╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝ ╚═╝╚═════╝ ╚═════╝    ╚═╝  
+                                                                         
+
+ [bright_yellow]╔══════════════════════════════╗[/]
+ [bright_yellow]║[/]    [bold cyan]DoS Testing Framework[/]     [bright_yellow]║[/]
+ [bright_yellow]║[/]   [bold red]Created by Ghiyas1337[/]     [bright_yellow]║[/]
+ [bright_yellow]╚══════════════════════════════╝[/]
+ [bright_yellow][[/][bold red]WARNING[/][bright_yellow]][/] [bold white]Educational Purposes Only[/]
+ [bright_yellow][[/][bold cyan]INFO[/][bright_yellow]][/] [bold white]Press Ctrl+C to Stop Attack[/]
+ [bright_yellow][[/][bold green]MODE[/][bright_yellow]][/] [bold white]Unlimited Threads | Auto-Timer[/]
+
 ⠀⠀⠀⠀⠀⠀[/]"""
     console.print(Align.center(banner))
 
